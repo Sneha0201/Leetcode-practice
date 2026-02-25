8a26285b87890185950583d0afc1f3dbc4e0ee8c
Sneha0201 <snehachaurasia0201@gmail.com>
2026-02-25 20:50:26 +0530

    Create 1356_sort_int_by_no_of_1'sbits.py

---
+++ Bit Manipulation/1356_sort_int_by_no_of_1'sbits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        def countbits(n):
+            count = 0
+            while n:
+                n &= (n - 1)
+                count += 1
+            return count
+        return sorted(arr, key= lambda x: (countbits(x), x))
