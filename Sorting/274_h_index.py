25c30f195e344ac522d581cb6c64b557f1894f46
Sneha0201 <snehachaurasia0201@gmail.com>
2026-03-01 20:10:28 +0530

    Create 274_h_index.py

---
+++ Sorting/274_h_index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort(reverse=True)
+        h = 0
+        for i in range(n):
+            if citations[i] >= i + 1:
+                h = i + 1
+            else:
+                break
+        return h
