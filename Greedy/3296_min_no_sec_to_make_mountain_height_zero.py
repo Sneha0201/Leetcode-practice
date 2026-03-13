a56cf8ce6924b02e19008e17d4e12ed441e44a66
Sneha0201 <snehachaurasia0201@gmail.com>
2026-03-13 12:26:07 +0530

    Create 3296_min_no_sec_to_make_mountain_height_zero.py

---
+++ Greedy/3296_min_no_sec_to_make_mountain_height_zero.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
+        def can_finish(T):
+            total = 0
+            for t in workerTimes:
+                x = int((math.sqrt(1 + 8 * T / t) - 1) // 2)
+                total += x
+                if total >= mountainHeight:
+                    return True
+            return False
+        left, right = 0, 10 ** 18
+        ans = right
+        while left <= right:
+            mid = (left + right) // 2
+            if can_finish(mid):
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
