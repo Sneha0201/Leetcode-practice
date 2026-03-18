1a5eee16b5d4833b97f2a2c364dfaa119fbb866d
Sneha0201 <snehachaurasia0201@gmail.com>
2026-03-18 21:15:21 +0530

    Create 86_partition_linklist.py

---
+++ Linklist/86_partition_linklist.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        small_dummy = ListNode(0)
+        large_dummy = ListNode(0)
+        small = small_dummy
+        large = large_dummy
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                large.next = head
+                large = large.next
+            head = head.next
+        large.next = None
+        small.next = large_dummy.next
+        return small_dummy.next
