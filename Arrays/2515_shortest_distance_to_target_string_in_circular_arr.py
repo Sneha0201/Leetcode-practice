a5354987e58099967fc2ddbe623f8a76dfe0ec86
Sneha0201 <snehachaurasia0201@gmail.com>
2026-04-15 19:05:30 +0530

    Create 2515_shortest_distance_to_target_string_in_circular_arr.py

---
+++ Arrays/2515_shortest_distance_to_target_string_in_circular_arr.py
@@ -0,0 +1,9 @@
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        n = len(words)
+        ans = float('inf')
+        for i in range(n):
+            if words[i] == target:
+                diff = abs(i - startIndex)
+                ans = min(ans, diff, n - diff)
+        return -1 if ans == float('inf') else ans
