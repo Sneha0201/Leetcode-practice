4d0b75d2a24e60687892f108ab3cdbefdc16a734
Sneha0201 <snehachaurasia0201@gmail.com>
2026-04-15 20:45:51 +0530

    Create 1480_running_sum_of_1-D_arr.py

---
+++ Arrays/1480_running_sum_of_1-D_arr.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = []
+        res.append(nums[0])
+        for i in range(1, n):
+            x = res[i - 1] + nums[i]
+            res.append(x)
+        return res
