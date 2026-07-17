3a915344e2f8ee51a98b7614855a431192c017c8
Sneha0201 <snehachaurasia0201@gmail.com>
2026-07-17 20:42:42 +0530

    Create 746_Min_Cost_Climbing_Stairs.py

---
+++ DP/746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,11 @@
+# Minimum cost climbing stairs
+cost = [1,100,1,1,1,100,1,1,100,1]
+def minCostClimbingStairs(cost):
+    n = len(cost)
+    dp = [0] * n
+    dp[0] = cost[0]
+    dp[1] = cost[1]
+    for i in range(2, n):
+        dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+    return min(dp[n - 1], dp[n - 2])
+print(minCostClimbingStairs(cost))
