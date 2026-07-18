64a33cd9cadefceb5434add5d64939f44210f0ad
Sneha0201 <snehachaurasia0201@gmail.com>
2026-07-18 21:35:43 +0530

    Create 322_Coin_change.py

---
+++ DP/322_Coin_change.py
@@ -0,0 +1,14 @@
+# Coin change
+coins = [1,2,5]
+amount = 11 
+def coinChange(coins, amount):
+    dp = [float("inf")] * (amount + 1)
+    dp[0] = 0
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    if dp[amount] == float("inf"):
+        return -1
+    return dp[amount]
+print(coinChange(coins, amount))
