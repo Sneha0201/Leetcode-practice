0739372bf91320f3452441008a0acd682242d42f
Sneha0201 <snehachaurasia0201@gmail.com>
2026-09-04 11:29:35 +0530

    Create 977_Square_of_sorted_array.py

---
+++ Two_Pointer/977_Square_of_sorted_array.py
@@ -0,0 +1,15 @@
+# Square of a sorted array
+nums = [-4, -1, 0, 3, 10]
+def sortedSquare(nums):
+    left = 0
+    right = len(nums) - 1
+    result = [0] * len(nums)
+    for i in range(len(nums) - 1, -1, -1):
+        if abs(nums[left]) > abs(nums[right]):
+            result[i] = nums[left] ** 2
+            left += 1
+        else:
+            result[i] = nums[right] ** 2
+            right -= 1
+    return result
+print(sortedSquare(nums))
