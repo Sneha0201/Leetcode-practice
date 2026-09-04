42af27c00a19a69b1246a16133d17c75b250ae77
Sneha0201 <snehachaurasia0201@gmail.com>
2026-09-04 17:13:15 +0530

    Create 845_longest_mountain_in_array.py

---
+++ Two_Pointer/845_longest_mountain_in_array.py
@@ -0,0 +1,20 @@
+# Longest Mountain in array
+arr = [2,1,4,7,3,2,5]
+def longestMountain(arr):
+    n = len(arr)
+    ans = 0
+    i = 1
+    while i < n - 1:
+        if arr[i - 1] < arr[i] > arr[i + 1]:
+            left = i
+            right = i
+            while left > 0 and arr[left - 1] < arr[left]:
+                left -= 1
+            while right < n - 1 and arr[right] > arr[right + 1]:
+                right += 1
+            ans = max(ans, right - left + 1)
+            i = right
+        else:
+            i += 1
+    return ans
+print(longestMountain(arr))
